e0ee2ba1813491b4b26fcace4620c0268bfc1d26
imrane ALI LAFKIH <imrane.laf@gmail.com>
2024-11-18 13:08:11 +0100

    fix: implement dynamic game fetching with route handler
    - Add single game fetching by ID functionality
    - Fix route handler exports according to Next.js conventions
    - Update API endpoint to handle both all games and single game requests

--- app/(dashboard)/games/[id]/page.tsx
+++ app/(dashboard)/games/[id]/page.tsx
@@ -1,9 +1,27 @@
 'use client'
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
-export default function Game() {
+export default  function Game() {
     const params = useParams();
     const id  = params.id; 
+
+   useEffect(()=>{
+    const Fdata = async ()=>{
+        try{
+        const data = await fetch(`/api/games?id=${id}`)
+        const alldata = await data.json()
+        console.log(alldata)
+        }catch(err){
+            console.log(err);
+            
+        }
+    }
+    Fdata()
+   }, [])
+    
+
+    
 
     return (
         <div>
--- app/api/games/route.ts
+++ app/api/games/route.ts
@@ -1,18 +1,60 @@
-// app/api/games/route.ts
+/* 
+  This function handles GET requests to fetch game data:
+  1. Checks for the presence of the 'id' parameter in the URL:
+     - If no 'id' is provided, it fetches all game data with their associated images.
+     - If 'id' is provided, it fetches the specific game data with its associated images.
+  2. Handles three states:
+     - Success: Returns the requested data as JSON.
+     - Not Found: Returns a 404 error if the specified game is not found.
+     - Error: Returns a 500 error for unexpected issues during data retrieval.
+*/  
+
 import { PrismaClient } from '@prisma/client'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const params = request.nextUrl.searchParams
+  const id = params.get('id')
+  console.log(id);
+  
+  
+  
+  
+
   try {
-    const games = await prisma.game.findMany({
+    if (!id) {
+      const data = await prisma.game.findMany({
+        include: {
+          images: true
+        }
+      })
+      return NextResponse.json(data)
+    }
+  
+    const data = await prisma.game.findUnique({
+      where: {
+        id: id
+      },
       include: {
         images: true
       }
     })
-    return NextResponse.json(games)
+    if (!data){
+      return NextResponse.json(
+        {error: 'Game Not Found'},
+        {status: 404}
+      )
+    }
+    return NextResponse.json(data)
+    
   } catch (error) {
-    return NextResponse.json({ error: 'Error fetching games' }, { status: 500 })
+    console.error('Error:', error)
+    return NextResponse.json(
+      { error: 'Error fetching games' },
+      { status: 500 }
+    )
   }
+  
 }
