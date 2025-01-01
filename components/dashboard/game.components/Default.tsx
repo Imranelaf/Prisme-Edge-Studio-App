85468def7c2621b129be15bd1d32e45fa0cacb8c
imrane ALI LAFKIH <imrane.laf@gmail.com>
2025-01-01 22:12:47 +0100

    Update the dependencies

--- components/dashboard/game.components/Default.tsx
+++ components/dashboard/game.components/Default.tsx
@@ -65,9 +65,7 @@ function Default() {
               />
             </button>
             <div className="w-full flex space-x-4">
-              <button className="w-auto bg-green-700 rounded-md hover:text-white hover:bg-green-900 p-2">
-                Modify
-              </button>
+
               <button
                 className="w-auto bg-red-700 rounded-md hover:text-white hover:bg-red-900 p-2"
                 onClick={() => handleDelete(item.id)}
